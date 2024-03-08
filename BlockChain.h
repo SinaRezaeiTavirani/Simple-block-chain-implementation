26ac011e3c414daab7c030d086085a9521bce396
SinaRezaeiTavirani <sinaezar@yahoo.com>
2024-03-08 12:47:42 +0330

    add and get latest function must be public

--- BlockChain.h
+++ BlockChain.h
@@ -5,11 +5,11 @@ class BlockChain
 {
 public: 
 	BlockChain();
+	void add_block(Block block);
+	Block get_latest_block();	
 
 private:
 	Block create_genesis_block();
-	Block get_latest_block();
-	void add_block(Block block);
 
 };
 
